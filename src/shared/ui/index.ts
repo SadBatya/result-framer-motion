bbd0b42bf7518f974496cd4f75d4fef25162841d
sadbatya <sadbatya@mail.ru>
2025-08-16 11:21:37 +0200

    feat

--- src/shared/ui/index.ts
+++
@@ -1,3 +0,0 @@
-export { Section } from "./Section/Section";
-export { ProgressBar } from "./ProgressBar/ProgressBar";
-export { Button, FramerButton } from "./Button/Button";
--- src/widgets/index.ts
+++
@@ -1,9 +0,0 @@
-export { SectionWelcome } from "./section-welcome/section-welcome";
-export { SectionAboutMe } from "./section-about-me/section-about-me";
-export { SectionButtons } from "./section-button/section-button";
-export { SectionNavigation } from "./section-navigation/section-navigation";
-export { SectionText } from "./section-text/section-text";
-export { SectionDrag } from "./section-drag/section-drag";
-export { SectionReorder } from "./section-reorder/section-reorder";
-export { SectionInView } from "./section-in-view/section-in-view";
-export { SectionPlan } from "./section-plan/section-plan";
